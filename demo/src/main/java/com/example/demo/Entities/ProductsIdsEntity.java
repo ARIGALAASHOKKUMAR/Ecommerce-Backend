bf30f68e23bfb808807919139421a15fed379c12
ARIGALA ASHOK KUMAR <ashok.arigala@apcfss.in>
2025-06-26 10:50:40 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/Entities/ProductsIdsEntity.java
@@ -0,0 +1,17 @@
+package com.example.demo.Entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="product_ids")
+@Data
+public class ProductsIdsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String user_id;
+    private Integer product_id;
+
+}
